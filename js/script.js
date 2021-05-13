c5a18f8138ec55f5b4504d06bf30aec54c030341
aananthv <aananthv@outlook.com>
2021-05-13 14:32:03 +0530

    feat: Add smooth rotation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 const section_container = document.getElementById('sections');
+const num_sections = section_container.childElementCount;
+const rotation_angle = 2 * Math.PI / num_sections;
 
-let rotation = 0;
+let current_section = 0;
+let loading = false;
+
 document.body.addEventListener('wheel', (e) => {
-    rotation += e.deltaY * 0.1;
-    section_container.style.transform = `rotate(${rotation}deg)`
+    if (!loading) {
+        loading = true;
+
+        if (e.deltaY < 0) {
+            current_section--;
+        } else if (e.deltaY > 0) {
+            current_section++;
+        }
+    
+        section_container.style.transform = `rotate(${rotation_angle * current_section}rad)`;
+
+        setTimeout(() => {loading = false}, 1000);
+    }
 })
